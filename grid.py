be2751f5d63950cea3593c89c60bc323b1295214
maestr01 <maestr01@wsc.edu>
2022-01-10 23:22:20 -0600

    Fix grid pattern, fill plots with colors

--- grid.py
+++ grid.py
@@ -9,25 +9,27 @@ def geometry():
 
     plots = []
 
+    w = w_grid/3
+    h = h_grid/3
+
     x1 = origin_x
     y1 = origin_y
 
-    w = w_grid/3
-    h = h_grid/3
+    x2 = w
+    y2 = h
 
     i = 0
 
     while i < 3:
-        y2 = y1 + h
         j = 0
         while j < 3:
-            x2 = x1 + w
-            coords = [[x1,y1],[x2,y1],[x1,y2],[x2,y2]]
+            coords = [x1,y1,x2,y2]
+            # coords = [[x1,y1],[x2,y1],[x1,y2],[x2,y2]]
             plots.append(coords)
-            x1 = x2
+            x1 = x2 + x1
             j+=1
         x1 = origin_x
-        y1 = y2
+        y1 = y2 + y1
         i+=1
 
     for p in plots:
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 '''
+!!! EPILEPSY WARNING: Lot's of flashing colors !!!
+
 This program is for populating a grid with 4 set of coordinates for each
 square inside of the grid. 
 Will be using this to better facilitate my Tic Tac Toe PG game
@@ -8,6 +10,8 @@ Date: 1/6/2022
 '''
 
 import pygame, sys
+
+from pygame.constants import MOUSEBUTTONDOWN
 
 import grid as g
 
@@ -21,22 +25,77 @@ red = 255,0,0
 
 scr = pygame.display.set_mode((size))
 
-
+clock = pygame.time.Clock()
 running = True
 
+change = True
+i = 0
+k = 0
+p = 0
+pChange = True
 while running:
+    pygame.Surface.fill(scr, (255,255,255))
+    clock.tick(7)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            k+=1
+            if k == 3:
+                k = 0
 
     plots = g.geometry()
 
-    for p in plots:
-        box = pygame.Rect(p[0][0],p[0][1],p[3][0],p[3][1])
-        # box = pygame.Rect(50,50,183,183)
-        pygame.draw.rect(scr,(255,255,),(box),2)
-    # box = pygame.Rect(50,50,183,183)
-    # pygame.draw.rect(scr,white,(box),2)
 
+    if change == True:
+        i+= 5
+    else:
+        i-= 5
+    if i >= 255:
+        change = False
+        i = 255
+    if i <= 0:
+        change = True
+        i = 0
+    bounds = pygame.Rect(plots[p][0],plots[p][1],plots[p][2],plots[p][3])
+    color = ()
+    if k == 0:
+        color = (255,255,i)
+    elif k ==1:
+        color = (255,i,255)
+    else:
+        color = (i,255,255)
+    box = pygame.draw.rect(scr,color,(bounds),0)
+
+    if pChange == True:
+        p+=1
+    else:
+        p-=1
+    if p == 8:
+        pChange = False
+    if p == 0:
+        pChange = True
     
+
+    # for p in plots:
+    #     if change == True:
+    #         i+= 90
+    #     else:
+    #         i-= 90
+    #     if i >= 255:
+    #         change = False
+    #         i = 255
+    #     if i <= 0:
+    #         change = True
+    #         i = 0
+    #     bounds = pygame.Rect(p[0],p[1],p[2],p[3])
+    #     color = ()
+    #     if k == 0:
+    #         color = (255,255,i)
+    #     elif k ==1:
+    #         color = (255,i,255)
+    #     else:
+    #         color = (i,255,255)
+    #     box = pygame.draw.rect(scr,color,(bounds),0)
+        
 
     pygame.display.update()
